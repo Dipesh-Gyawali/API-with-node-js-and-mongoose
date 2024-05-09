453d0a10b486bf703f02f5c76da705b6f379844c
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-09 06:15:46 +0545

    feat/create method added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("./config");
+const Product = require("./product");
+const app = express();
+
+app.use(express.json());
+
+app.post("/create", async (req, resp) => {
+  let data = new Product(req.body);
+  const result = await data.save();
+  resp.send(result);
+});
+app.listen(5173);
